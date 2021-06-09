eaceda7569b097c6f9ad2a0889aee788d4418f55
mattdowning314 <downing.matthew123@gmail.com>
2021-06-09 16:17:23 -0400

    added js code to control learn more buttons for experience and projects pages

--- js/script.js
+++ js/script.js
@@ -16,3 +16,31 @@ function website() {
 function redirectHomePage() {
 	window.location.href="https://matt-downing.com"
 }
+function redirectjob(company) {
+	if (company == "Festo") {
+		window.location.href="https://matt-downing.com/webpages/experience-festo.html"
+	}
+	else if (company == "Twiddy") {
+		window.location.href="https://matt-downing.com/webpages/experience-twiddy.html"
+	}
+	else if (company == "DigitalLumens") {
+		window.location.href="https://matt-downing.com/webpages/experience-digitallumens.html"
+	}
+	else if (company == "DuPont") {
+		window.location.href="https://matt-downing.com/webpages/experience-dupont.html"
+	}
+}
+function redirectproject(project) {
+	if (project == "8bit") {
+		window.location.href="https://matt-downing.com/webpages/projects-8bit.html"
+	}
+	else if (project == "sunrise") {
+		window.location.href="https://matt-downing.com/webpages/projects-sunrise.html"
+	}
+	else if (project == "binaryclock") {
+		window.location.href="https://matt-downing.com/webpages/projects-binaryclock.html"
+	}
+	else if (project == "bootcamp") {
+		window.location.href="https://matt-downing.com/webpages/projects-bootcamp.html"
+	}
+}
