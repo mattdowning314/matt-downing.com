cc086c6b7e9bc4f543498961c7eb03ccd068bfcb
mattdowning314 <downing.matthew123@gmail.com>
2021-06-09 13:09:57 -0400

    added js directory with sript.js file to hold javascript code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function redirectProjects() {
+	window.location.href="https://matt-downing.com/webpages/projects.html"
+}
+function redirectExperience() {
+	window.location.href="https://matt-downing.com/webpages/experience.html"
+}  
+function linkedin() {
+	window.location.href="https://www.linkedin.com/in/matthew-downing-0455a5177/"
+}  
+function github() {
+	window.location.href="https://github.com/mattdowning314/"
+}  
+function website() {
+	window.location.href="https://matt-downing.com"
+}
